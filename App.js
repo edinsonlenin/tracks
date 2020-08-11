2daa7d78efd01028fa928f694cf63d282624143f
edinsonlenin <edinsonlen@hotmail.com>
2020-08-11 17:33:24 -0500

    subiendo

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
+import "react-native-gesture-handler";
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-import AccountScreen from './src/screens/AccountScreen';
-import SigninScreen from './src/screens/SigninScreen';
-import SignupScreen from './src/screens/SignupScreen';
-import TrackCreateScreen from './src/screens/TrackCreateScreen';
-import TrackDetailScreen from './src/screens/TrackDetailScreen';
-import TrackListScreen from './src/screens/TrackListScreen';
+function LoginFlow() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Signin" component={SigninScreen} />
+    </Stack.Navigator>
+  );
+}
 
+const TrackListFlow = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="TrackList" component={TrackListScreen} />
+    <Stack.Screen name="TrackDetail" component={TrackDetailScreen} />
+  </Stack.Navigator>
+);
+
+const MainFlow = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="TrackListFlow" component={TrackListFlow} />
+    <Tab.Screen name="TrackCreate" component={TrackCreateScreen} />
+    <Tab.Screen name="Account" component={AccountScreen} />
+  </Tab.Navigator>
+);
+
+export default function App() {
+  const [isLogued, setIsLogued] = useState(false);
+  return (
+    <NavigationContainer>
+      {isLogued ? MainFlow() : LoginFlow()}
+    </NavigationContainer>
+  );
+}
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const SingupScreen = props => {
-    return <Text style={{fontSize: 48}}>Singup Screen</Text>;
+const SingupScreen = ({ navigation, setIsLogued }) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>Singup Screen</Text>
+      <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')} />
+      <Button title="Go to Main Flow" onPress={() => setIsLogued(true)} />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default SingupScreen;
